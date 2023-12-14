f83f5964198a6f020fbd11ee6a01e89cdf3ad6ef
Ahmed Dowidar <ahmeddwedar725@gmail.com>
2023-12-14 11:32:34 +0200

    fix error messages in login and otp login and create arabic names for voter when application loaded

--- src/main/java/com/electronic/voting/config/DataLoader.java
+++ src/main/java/com/electronic/voting/config/DataLoader.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Date;
+import java.util.Random;
 
 @Configuration
 public class DataLoader {
@@ -15,19 +16,34 @@ public class DataLoader {
     @Bean
     CommandLineRunner initDatabase(VoterRepository voterRepository) {
         return args -> {
-            /*for (int i = 0; i < 10; i++) { // Create 10 dummy voters
+            String[] arabicNames = {"علي", "أحمد", "فاطمة", "سارة", "يوسف", /* more names */ };
+            Random random = new Random();
+            for (int i = 0; i < 10; i++) { // Create 10 dummy voters
                 Voter voter = new Voter();
-                voter.setName("Voter " + RandomStringUtils.randomAlphabetic(5));
+                // Construct a random Arabic name
+                StringBuilder nameBuilder = new StringBuilder();
+                for (int j = 0; j < 4; j++) {
+                    nameBuilder.append(arabicNames[random.nextInt(arabicNames.length)]);
+                    if (j < 3) {
+                        nameBuilder.append(" "); // add space between names
+                    }
+                }
+                voter.setName(nameBuilder.toString());
+
                 voter.setDateOfBirth(new Date());
                 voter.setAddress("1234 Main St");
                 voter.setEmail("example" + i + "@mail.com");
                 voter.setPhoneNumber("123-456-" + RandomStringUtils.randomNumeric(4));
-                voter.setNationalId(RandomStringUtils.randomNumeric(10));
-                voter.setAge(18 + (int) (Math.random() * 30));
+
+                // Generate a 16-digit national ID starting with 29
+                String nationalId = "29" + RandomStringUtils.randomNumeric(14);
+                voter.setNationalId(nationalId);
+
+                voter.setAge(18 + random.nextInt(30)); // Age between 18 and 48
                 // Add other fields as necessary
 
                 voterRepository.save(voter);
-            }*/
+            }
         };
     }
 }
